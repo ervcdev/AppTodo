6ae817eb910bc069c6bb8540f64ab4b1eba4124f
Ervin Castro Perez <ervindevc@gmail.com>
2024-03-11 22:22:49 -0500

    create seetup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TodoCounter/>
+      <TodoSearch/>
+      
+         
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
     </div>
+  );
+}
+
+function TodoItem() {
+  return (
+    <li>
+      <span>v</span>
+      <p>Vender</p>
+      <spna>x</spna>
+    </li>
   );
 }
 
