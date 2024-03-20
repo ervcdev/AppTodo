51f27386149f005d8274395196eeb4b3a74d1d7d
Ervin Castro Perez <ervindevc@gmail.com>
2024-03-19 23:27:54 -0500

    create completeTodo

--- src/App.js
+++ src/App.js
@@ -15,19 +15,37 @@ const defaultTodos = [
 function App() {
 
   const [todos, setTodos] = React.useState(defaultTodos)
-  const [searchValue, setSearchValue] = React.useState(" ");
-  console.log('buscaron',searchValue);
+  const [searchValue, setSearchValue] = React.useState("");
 
   const completedTodos = todos.filter(todo => !!todo.completed).length ;
   const totalTodos = todos.length;
 
-  const searchTodos = todos.filter(
+  const searchedTodos = todos.filter(
     (todo) => {
       const todoText = todo.text.toLowerCase();
       const searchText = searchValue.toLowerCase();
       return todoText.includes(searchText)
     }
   );
+
+  const completeTodo = (text) => { 
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text === text
+    );
+    newTodos[todoIndex].completed = true;
+    setTodos(newTodos)
+  }
+
+  const deleteTodo = (text) => { 
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text === text
+    );
+    newTodos.splice(todoIndex,1);
+    setTodos(newTodos)
+  }
+
 
   return (
     <>
@@ -42,11 +60,15 @@ function App() {
       />
 
       <TodoList>
-        {searchTodos.map((todo) => (
+        {searchedTodos.map((todo) => (
           <TodoItem 
           key={todo.text} 
           text={todo.text} 
           completed={todo.completed}
+          onComplete={() => completeTodo(todo.text)}
+          onDelete={() => deleteTodo(todo.text)}
+
+
           
           />
         ))}
